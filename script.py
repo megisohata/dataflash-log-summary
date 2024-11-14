09de465bfb74a78daae2c54f0cf0ea312a8c5adb
Meg <msi32@cornell.edu>
2024-11-14 15:21:44 -0500

    Calculate number of flights and flight time

--- script.py
+++ script.py
@@ -1,8 +1,16 @@
 from pymavlink import mavutil
 
-file = 'logs/00000075.BIN'
+file = 'logs/00000085.BIN'
 
 mavlog = mavutil.mavlink_connection(file, dialect='ardupilotmega', robust_parsing=True)
+
+flying = False
+start = 0
+end = 0
+flights = 0
+flightTime = 0
+
+buffer = 1
 
 while True:
     message = mavlog.recv_match()
@@ -10,4 +18,19 @@ while True:
     if not message:
         break
 
-    print(f"{message.get_type()}: {message.to_dict()}")
+    messageType = message.get_type()
+
+    if (messageType == 'POS'):
+        if (not flying and message.to_dict()['RelHomeAlt'] > buffer):
+            flying = True
+            flights += 1
+            start = message.to_dict()['TimeUS']
+        elif (flying and message.to_dict()['RelHomeAlt'] <= buffer):
+            flying = False
+            end = message.to_dict()['TimeUS']
+            flightTime += end - start
+            
+    print(f"{messageType}: {message.to_dict()}")
+    
+print('Flights: ' + str(flights))
+print('Flight Time: ' + str(flightTime / 10e5) + 's')
