c471202781aa1a085f62b6f03676ac9a23593f92
Meg <msi32@cornell.edu>
2024-11-15 17:39:30 -0500

    Make more user friendly dquote> dquote> - Add auto flight calculation dquote> - Add ability to print every single message dquote> - Add help menu

--- script.py
+++ script.py
@@ -1,14 +1,16 @@
 from pymavlink import mavutil
 import matplotlib.pyplot as plt
 
-file = 'logs/00000075.BIN'
+file = 'logs/00000085.BIN'
 
 data = {
+    'all': [],
     'flight_summary': [],
-    'battery_voltage': []
+    'battery_voltage': [],
+    'flights': []
 }
 
-flight_summary_types = ['STAT', 'POS', 'MODE']
+flight_summary_types = { 'STAT', 'POS', 'MODE' }
 
 def parse_log(file):
     mavlog = mavutil.mavlink_connection(file, dialect='ardupilotmega', robust_parsing=True)
@@ -20,92 +22,120 @@ def parse_log(file):
 
         if not message:
             break
-        
-        if message.get_type() in flight_summary_types:
-            data['flight_summary'].append(message.to_dict())
 
-        if message.get_type() == 'BAT':
-            data['battery_voltage'].append(message.to_dict())
+        message = message.to_dict()
+        
+        data['all'].append(message)
+
+        if message['mavpackettype'] in flight_summary_types:
+            data['flight_summary'].append(message)
+
+        if message['mavpackettype'] == 'BAT':
+            data['battery_voltage'].append(message)
         
         total_messages += 1
         print(f'Processing {total_messages} messages...', end='\r')
 
-        # print(f"{message.get_type()}: {message.to_dict()}")
+        # print(f'{message['mavpackettype']}: {message}')
     
     print(f'Finished processing {total_messages} messages.')
 
-def flight_summary(data):
-    isArmed = False
-    isFlying = False
-    isAuto = False
+def flight_summary(messages):
+    is_armed = False
+    is_flying = False
+    is_auto_mode = False
+    is_auto_flight = False
 
-    flightStart = 0
-    flightEnd = 0
-    autoFlightStart = 0
-    autoFlightEnd = 0
+    flight_start = 0
+    flight_end = 0
+    auto_flight_start = 0
+    auto_flight_end = 0
 
-    totalFlights = 0
-    totalFlightTime = 0
-    totalAutoTime = 0
+    total_flights = 0
+    total_flight_time = 0
+    total_auto_flights = 0
+    total_auto_time = 0
 
-    flightAltBuffer = 0.1
+    flight_alt_buffer = 0.1
 
-    for message in data:
+    for message in messages:
         type = message['mavpackettype']
 
         if (type == 'STAT'):
 
-            if (not isArmed and message['isFlying'] == 1):
-                isArmed = True
-            elif (isArmed and message['isFlying'] == 0):
-                isArmed = False
+            if (not is_armed and message['isFlying'] == 1):
+                is_armed = True
+            elif (is_armed and message['isFlying'] == 0):
+                is_armed = False
 
         elif (type == 'POS'):
-            if (isArmed and not isFlying and message['RelHomeAlt'] > flightAltBuffer):
-                isFlying = True
-                totalFlights += 1
-                flightStart = message['TimeUS']
+            if (is_armed and not is_flying and message['RelHomeAlt'] > flight_alt_buffer):
+                is_flying = True
+                total_flights += 1
+                flight_start = message['TimeUS']
+                data['flights'].append(flight_start)
 
-                if (isAuto):
-                    autoFlightStart = message['TimeUS']
+                if is_auto_mode:
+                    auto_flight_start = message['TimeUS']
 
-            elif (isArmed and isFlying and message['RelHomeAlt'] <= flightAltBuffer):
-                isFlying = False
-                flightEnd = message['TimeUS']
-                totalFlightTime += flightEnd - flightStart
+                    if not is_auto_flight:
+                        is_auto_flight = True
+                        total_auto_flights += 1
 
-                if (isAuto):
-                    autoFlightEnd = message['TimeUS']
-                    totalAutoTime += autoFlightEnd - autoFlightStart
+            elif (is_armed and is_flying and message['RelHomeAlt'] <= flight_alt_buffer):
+                is_flying = False
+                flight_end = message['TimeUS']
+                data['flights'].append(flight_end)
+                total_flight_time += flight_end - flight_start
+
+                if (is_auto_mode):
+                    auto_flight_end = message['TimeUS']
+                    total_auto_time += auto_flight_end - auto_flight_start
+
+                    if is_auto_flight:
+                        is_auto_flight = False
         
         elif (type == 'MODE'):
             if (message['Mode'] == 10):
-                isAuto = True
+                is_auto_mode = True
                 
-                if (isFlying):
-                    autoFlightStart = message['TimeUS']
+                if (is_flying):
+                    auto_flight_start = message['TimeUS']
+
+                    if not is_auto_flight:
+                        is_auto_flight = True
+                        total_auto_flights += 1
             else:
-                if (isAuto):
-                    isAuto = False
+                if (is_auto_mode):
+                    is_auto_mode = False
 
-                    if (isFlying):
-                        autoFlightEnd = message['TimeUS']
-                        totalAutoTime += autoFlightEnd - autoFlightStart
+                    if (is_flying):
+                        auto_flight_end = message['TimeUS']
+                        total_auto_time += auto_flight_end - auto_flight_start
 
-    print('Total Flights: ' + str(totalFlights))
-    print('Total Flight Time: ' + str(totalFlightTime / 10e5) + 's')
-    print('Auto Flight Time: ' + str(totalAutoTime / 10e5) + 's')
-    print('Manual Flight Time: ' + str((totalFlightTime - totalAutoTime) / 10e5) + 's')
+                        if is_auto_flight:
+                            is_auto_flight = False
 
-def battery_voltage(data):
-    batteryVoltageTimes = []
-    batteryVoltages = []
+    print('Key flight data (for DesOps):\n' + 
+          '\tTotal Flights: ' + str(total_flights) + '\n' + 
+          '\tTotal Auto Flights: ' + str(total_auto_flights) + '\n' + 
+          '\tTotal Flight Time: ' + str(total_flight_time / 10e5) + 's' + '\n' + 
+          '\tAuto Flight Time: ' + str(total_auto_time / 10e5) + 's' + '\n' +
+          '\tManual Flight Time: ' + str((total_flight_time - total_auto_time) / 10e5) + 's')
 
-    for message in data:
-        batteryVoltageTimes.append(message['TimeUS'] / 10e5)
-        batteryVoltages.append(message['Volt'])
+def print_all(messages):
+    for message in messages:
+        print(message)
+
+def battery_voltage(messages):
+    batter_voltage_times = []
+    batter_voltages = []
+
+    for message in messages:
+        batter_voltage_times.append(message['TimeUS'] / 10e5)
+        batter_voltages.append(message['Volt'])
     
-    plt.plot(batteryVoltageTimes, batteryVoltages)
+    plt.plot(batter_voltage_times, batter_voltages)
     plt.xlabel('Time (s)')
     plt.ylabel('Voltage (V)')
     plt.show()
@@ -116,9 +146,15 @@ if __name__ == '__main__':
     flight_summary(data['flight_summary'])
 
     while True:
-        user = input('Enter: ')
+        user = input('Enter a command: ')
 
         if user == 'exit':
             break
+        elif user == 'help':
+            print('Available commands:')
+        elif user == 'all':
+            print_all(data['all'])
         elif user == 'bat':
             battery_voltage(data['battery_voltage'])
+        else:
+            print('Please enter a valid command. For a list of all commands, enter [help].')
